fd3dad2de72625dbe635c459b5692e530d1cbc9d
John Petrocik <john@petrocik.net>
2017-11-12 21:45:31 -0800

    Change BOARD DEFINE version to all 3.x boards

--- firmware/configuration.h
+++ firmware/configuration.h
@@ -1,8 +1,8 @@
-#define BOARD_V3_0_WHITTIER
+#define BOARD_V3_X_WHITTIER
 
 /**
  * A very long debounce delay, so if the user accidentally
- * presses the button multiple time, it's not interpreted 
+ * presses the button multiple time, it's not interrupted 
  * as multiple presses
  */
 #define DEBOUNCE_DELAY 1000
@@ -17,7 +17,7 @@
  */
 #define GATE_RELEASE_ADJUSTMENT 70
 
-#ifdef BOARD_V3_0_WHITTIER 
+#ifdef BOARD_V3_X_WHITTIER 
   #define GATE 6
   #define LIGHT_RED 5
   #define LIGHT_YELLOW_1 3
@@ -28,7 +28,7 @@
   #define TIMER 0
 #endif
 
-#ifdef BOARD_V3_0
+#ifdef BOARD_V3_X
   #define GATE 6
   #define LIGHT_RED 2
   #define LIGHT_YELLOW_1 5
@@ -39,29 +39,6 @@
   #define TIMER 0
 #endif
 
-
-#ifdef BOARD_V2_3 
-  #define GATE 6
-  #define LIGHT_RED 5 
-  #define LIGHT_YELLOW_1 4
-  #define LIGHT_YELLOW_2 3
-  #define LIGHT_GREEN 2
-  #define SPEAKER 9
-  #define START 12
-  #define TIMER 0
-#endif
-
-
-#ifdef BOARD_V2_2 
-  #define GATE 2
-  #define LIGHT_RED 3 
-  #define LIGHT_YELLOW_1 4
-  #define LIGHT_YELLOW_2 5
-  #define LIGHT_GREEN 6
-  #define SPEAKER 9
-  #define START 7
-  #define TIMER 0
-#endif
 
 /**
  * Changable for boards where UART is a SerialX
